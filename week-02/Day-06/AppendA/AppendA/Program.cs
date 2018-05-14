81fa7f295e68f717de84e745d54baeaa521538b2
Kurti Peter <kurtipeter90@gmail.com>
2018-05-14 14:48:56 +0200

    core(AppendA)

---
+++ week-02/Day-06/AppendA/AppendA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `animals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+
+            string[] animals = { "kuty", "macsk", "cic" };
+
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animals[i] = animals[i] + "a";
+            }
+
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine(animals[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
