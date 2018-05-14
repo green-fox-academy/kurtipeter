d210b1875ac6281554b57f6babc825f6cea22d0b
Kurti Peter <kurtipeter90@gmail.com>
2018-05-14 14:07:14 +0200

    core(IncrementElement): new file

---
+++ week-02/Day-06/IncrementElement/IncrementElement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            //- Increment the third element
+            //- Print the third element
+            int[] t = { 1, 2, 3, 4, 5 };
+
+            t[2]++;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                Console.WriteLine(t[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
