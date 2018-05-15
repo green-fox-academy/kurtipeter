157f020e434651ad9d5cc899a5ec70174cb83b7c
Kurti Peter <kurtipeter90@gmail.com>
2018-05-15 16:12:37 +0200

    add(ElementFinder)

---
+++ week-02/Day-02/ElementFinder/ElementFinder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElementFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine(ContainsSeven(List));
+            Console.ReadLine();
+            // Write a method that checks if the arrayList contains "7" if it contains return "Hoorray" otherwise return "Noooooo"
+            // The output should be: "Noooooo"
+            // Do this again with a different solution using different list methods!
+        }
+
+       public static string ContainsSeven(List<int> inputList)
+        {
+            int itHasASeven = 0;
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (inputList[i] == 7 && itHasASeven == 0)
+                {
+                    itHasASeven++;
+                    return "Hoorray";
+                }
+            }
+            return "Nooooo";
+        }
+    }
+}
