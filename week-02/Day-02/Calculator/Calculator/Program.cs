d641a60c911e21d4fc67fb849d25f38c58df2388
Kurti Peter <kurtipeter90@gmail.com>
2018-05-15 23:54:30 +0200

    add(HeWillNever)

---
+++ week-02/Day-02/Calculator/Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me something to calculate in the following form: \"+ x y\"");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(Calculate(userInput));
+            Console.ReadLine();
+        }
+
+        public static string Calculate(string input)
+        {
+            string[] splitInput = input.Split();
+            string calcOperator = splitInput[0];
+            int firstNumber = Int32.Parse(splitInput[1]);
+            int secondNumber = Int32.Parse(splitInput[2]);
+
+            if ("+" == calcOperator)
+            {
+                int solution = firstNumber + secondNumber;
+                return "The answer is: " + solution;
+            }
+            else if ("-" == calcOperator)
+            {
+                int solution = firstNumber - secondNumber;
+                return "The answer is: " + solution;
+            }
+            else if ("*" == calcOperator)
+            {
+                int solution = firstNumber * secondNumber;
+                return "The answer is: " + solution;
+            }
+            else if ("/" == calcOperator)
+            {
+                if (secondNumber != 0)
+                {
+                    double solution = Convert.ToDouble(firstNumber) - secondNumber;
+                    return "The answer is: " + solution;
+                }
+                else
+                {
+                    return "You are not allowed to make division with 0";
+                }
+            }
+            else if ("%" == calcOperator)
+            {
+                int solution = firstNumber - secondNumber;
+                return "The answer is: " + solution;
+            }
+            else
+            {
+                return "Your form was not valid, try again with using the right form this time.";
+            }
+
+        }
+    }
+}
