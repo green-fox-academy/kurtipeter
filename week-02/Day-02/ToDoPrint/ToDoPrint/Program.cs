4b7d7de35fae0ba3df3eef56315967c9c9b76576
Kurti Peter <kurtipeter90@gmail.com>
2018-05-15 12:36:59 +0200

    add(ToDoPrint)

---
+++ week-02/Day-02/ToDoPrint/ToDoPrint/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace ToDoPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indentation
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+            todoText = todoText.Insert(0, "My todo:\n");
+            todoText += " - Download games\n";
+            todoText += "\t- Diablo";
+
+            Console.WriteLine(todoText);
+            Console.ReadLine();
+        }
+    }
+}
