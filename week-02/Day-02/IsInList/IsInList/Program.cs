a8fa9bbb7d12d02aaa593cde27e2497569f1722f
Kurti Peter <kurtipeter90@gmail.com>
2018-05-15 16:43:12 +0200

    add(IsInList)

---
+++ week-02/Day-02/IsInList/IsInList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsInList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+            List<int> checkThisNumbers = new List<int> { 4, 8, 12, 16, 17 };
+
+            // Check if list contains all of the following elements: 4,8,12,16
+            // Create a method that accepts list as an input
+            // it should return "true" if it contains all, otherwise "false"
+            Console.WriteLine(CheckNums(list, checkThisNumbers));
+            Console.ReadLine();
+        }
+
+        public static bool CheckNums(List<int> list, List<int> checkList)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < checkList.Count; i++)
+            {
+                if (list.Contains(checkList[i]))
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == checkList.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
