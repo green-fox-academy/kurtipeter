7c5ce64d543b5e217f214b77c8b510b31b7dc7f7
Kurti Peter <kurtipeter90@gmail.com>
2018-05-16 17:04:46 +0200

    add(ReversedOrder)

---
+++ week-02/Day-03/ReversedOrder/ReversedOrder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RevOrder("needOrderReversing.txt"));
+            Console.ReadLine();
+        }
+
+        public static string RevOrder(string path)
+        {
+            string[] content = File.ReadAllLines(path);
+            string revOrderCont = "";
+
+            for (int j = content.Length - 1; j >= 0; j--)
+            {
+                revOrderCont += content[j];
+                if (j != 0)
+                {
+                    revOrderCont += "\n";
+                }                
+            }
+
+            StreamWriter sw = new StreamWriter("orderReversed.txt");
+            sw.WriteLine(revOrderCont);
+            sw.Dispose();
+
+            return revOrderCont;
+        }
+    }
+}
