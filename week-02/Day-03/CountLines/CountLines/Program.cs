47094b5b9a92088164d435b79b4fc4f6a94e8306
Kurti Peter <kurtipeter90@gmail.com>
2018-05-16 12:18:41 +0200

    add(CountLines)

---
+++ week-02/Day-03/CountLines/CountLines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"my-file.txt";
+            try
+            {
+                string[] content = File.ReadAllLines(path);
+
+                int lineCount = 0;
+
+                foreach (string item in content)
+                {
+                    lineCount++;
+                }
+
+                Console.WriteLine(lineCount);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
