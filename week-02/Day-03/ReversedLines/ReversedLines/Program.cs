ef0b804a6f108469145056902e5bb99104d3e78a
Kurti Peter <kurtipeter90@gmail.com>
2018-05-16 16:52:47 +0200

    add(ReversedLines)

---
+++ week-02/Day-03/ReversedLines/ReversedLines/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ReversedLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RevLines("needreversing.txt"));
+            Console.ReadLine();
+        }
+
+        public static string RevLines(string path)
+        {
+            string[] content = File.ReadAllLines(path);
+            string revCont = "";
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                for (int j = content[i].Length - 1; j >= 0; j --)
+                {
+                    revCont += content[i][j];
+                }
+                revCont += "\n";
+            }
+
+            StreamWriter sw = new StreamWriter("reversed.txt");
+            sw.WriteLine(revCont);
+            sw.Dispose();
+
+            return revCont;
+        }
+    }
+}
