dc606b9f629729a5d46a1513c3b22f0854f7cf11
Kurti Peter <kurtipeter90@gmail.com>
2018-05-16 09:04:32 +0200

    fix(directory) name change

--- week-01/Day-05/29_DrawPyramid.cs
+++ week-01/Day-05/29_DrawPyramid.cs
@@ -6,40 +6,24 @@ namespace GreenFox
     {
         static void Main(string[] args)
         {
-            int squareSize = 0;
-            Console.WriteLine("What should be the size of the square?");
-            squareSize = Int32.Parse(Console.ReadLine());
 
-            for (int i = 0; i < squareSize; i++)
+        	int pyramidHeight = 0;
+            Console.WriteLine("What should be the size of the pyramid?");
+            pyramidHeight = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < pyramidHeight; i++)
             {
-                if ((i == 0) || (i == squareSize - 1))
+                for (int j = pyramidHeight; j > i; j--)
                 {
-                    for (int j = 0; j < squareSize; j++)
-                    {
-                        Console.Write("%");
-                    }
-                    Console.WriteLine("");
+                    Console.Write(" ");
                 }
-                else
+                for (int x = 0; x < i + i + 1; x++)
                 {
-                    for (int x = 0; x < squareSize; x++)
-                    {
-                        if ((x == 0) || (x == squareSize - 1))
-                        {
-                            Console.Write("%");
-                        }
-                        else if (x == i)
-                        {
-                            Console.Write("%");
-                        }
-                        else
-                        {
-                            Console.Write(" ");
-                        }
-                    }
-                    Console.WriteLine(" ");
+                    Console.Write("*");
                 }
+                Console.WriteLine("");
             }
+            Console.ReadLine();
         }
     }
 }
---
+++ week-02/Day-01/Palindrome-searcher/Palindrome-searcher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Palindrome_searcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "baabnanan";
+            Console.ReadLine();
+        }
+
+        public static List<string> PalindromeSearcher(string inputString)
+        {
+
+            List<int> palindromeIndexes = new List<int>();
+
+            for (int i = 1; i < inputString.Length; i++)
+            {
+                if (inputString[i] == inputString[i + 1])
+                {
+
+                }
+            }
+         
+        }
+    }
+}
