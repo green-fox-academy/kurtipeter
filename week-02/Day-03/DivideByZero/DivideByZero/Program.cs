dbd6ee5023d84b6d1c290341d7afe1fa8688717f
Kurti Peter <kurtipeter90@gmail.com>
2018-05-16 11:40:15 +0200

    add(DivideByZero)

---
+++ week-02/Day-03/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // It should print "fail" if the parameter is 0
+            Console.WriteLine(TenDivider(3));
+            Console.WriteLine(TenDivider(0));
+            Console.ReadLine();
+
+        }
+
+        public static string TenDivider(int inputNum)
+        {
+            if (inputNum == 0)
+            {
+                return "fail";
+            }
+            else
+            {
+                double solution = 10.0 / inputNum;
+                return solution.ToString();
+            }
+        }
+    }
+}
