b7afd24497ef493039f6ea78d3811450fa8f583d
Kurti Peter <kurtipeter90@gmail.com>
2018-05-16 13:39:30 +0200

    add(WriteSingleLine)

---
+++ week-02/Day-03/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open a file called "my-file.txt"
+            // Write your name in it as a single line
+            // If the program is unable to write the file,
+            // then it should print an error message like: "Unable to write file: my-file.txt"
+
+            NameWriter();
+            string[] check = File.ReadAllLines("my-file.txt");
+            Console.WriteLine(check[0]);
+            Console.ReadLine();
+        }
+
+
+        public static void NameWriter()
+        {
+
+            try
+            {
+                StreamWriter sw = new StreamWriter("my-file.txt");
+                sw.WriteLine("Kurtipeti");
+                sw.Dispose();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unbale to write in the file");
+            }
+        }
+    }
+}
