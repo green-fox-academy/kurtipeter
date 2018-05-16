ddcab1876ab167199c8a5a4d228b61581e1abc48
Kurti Peter <kurtipeter90@gmail.com>
2018-05-16 16:40:56 +0200

    add(DuplicatedChars)

---
+++ week-02/Day-03/DuplicatedChars/DuplicatedChars/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DuplicatedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DelDupChars("duplicated-chars.txt"));
+            Console.ReadLine();
+        }
+
+        public static string DelDupChars(string path)
+        {
+            string[] content = File.ReadAllLines(path);
+            string croppedCont = "";
+
+            for (int i = 0; i < content.Length; i ++)
+            {
+                for (int j = 0; j < content[i].Length; j += 2)
+                {
+                    croppedCont += content[i][j];
+                }
+                croppedCont += "\n";
+            }
+
+            StreamWriter sw = new StreamWriter("croppedTXT.txt");
+            sw.WriteLine(croppedCont);
+            sw.Dispose();
+
+            return croppedCont;
+        }
+    }
+}
