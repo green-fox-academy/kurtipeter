231db6264e2d5ae3396f2045abcb437bd7121337
Kurti Peter <kurtipeter90@gmail.com>
2018-05-17 14:24:16 +0200

    add(GoToCenter)

---
+++ week-02/Day-04/GoToCenter/GoToCenter/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace GoToCenter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawLines(foxDraw, 3);
+        }
+
+        public static void DrawALineToCenter(FoxDraw foxDraw, int x, int y)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(x, y, foxDraw.Canvas.Width / 2, foxDraw.Canvas.Height / 2);
+        }
+
+        public static void DrawLines(FoxDraw foxDraw, int numberOfLines)
+        {
+            for (int i = 0; i < numberOfLines; i ++)
+            {
+                DrawALineToCenter(foxDraw, i *40 , i * 75);
+            }
+        }
+    }
+}
