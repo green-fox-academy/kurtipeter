8ddaee5647f60a3a8974f578eab71337e529df7b
Kurti Peter <kurtipeter90@gmail.com>
2018-05-17 15:48:17 +0200

    chore(CenteredSquare)

---
+++ week-02/Day-04/CenteredSquare/CenteredSquare/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace CenteredSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawASquare(foxDraw);
+
+        }
+        public static void DrawASquare(FoxDraw foxDraw)
+        { 
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(foxDraw.Canvas.Width / 2 - 5, foxDraw.Canvas.Height / 2 -5, foxDraw.Canvas.Width / 2 - 5, foxDraw.Canvas.Height / 2 + 5);
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(foxDraw.Canvas.Width / 2 - 5, foxDraw.Canvas.Height / 2 - 5, foxDraw.Canvas.Width / 2 + 5, foxDraw.Canvas.Height / 2 - 5);
+
+            foxDraw.StrokeColor(Colors.Purple);
+            foxDraw.DrawLine(foxDraw.Canvas.Width / 2 + 5, foxDraw.Canvas.Height / 2 - 5, foxDraw.Canvas.Width / 2 + 5, foxDraw.Canvas.Height / 2 + 5);
+        
+            foxDraw.StrokeColor(Colors.Blue);
+            foxDraw.DrawLine(foxDraw.Canvas.Width / 2 - 5, foxDraw.Canvas.Height / 2 + 5, foxDraw.Canvas.Width / 2 + 5, foxDraw.Canvas.Height / 2 + 5);
+        }
+    }
+}
