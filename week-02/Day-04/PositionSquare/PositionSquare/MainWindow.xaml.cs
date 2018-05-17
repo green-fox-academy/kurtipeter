108660281f2c49f57e64c83e65542bd18be64f1a
Kurti Peter <kurtipeter90@gmail.com>
2018-05-17 17:04:18 +0200

    chore(PositionSquare)

---
+++ week-02/Day-04/PositionSquare/PositionSquare/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace PositionSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawSquareInPosition(foxDraw, 0,0);
+            DrawSquareInPosition(foxDraw, 150,150);
+            DrawSquareInPosition(foxDraw, 243,62);
+        }
+
+        public static void DrawSquareInPosition(FoxDraw foxDraw, int x, int y)
+        {
+            Random rnd = new Random();
+            for (int i = 1; i < 4 + 1; i++)
+            {
+                foxDraw.FillColor(Color.FromRgb((byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255)));
+
+                foxDraw.DrawRectangle(x, y, 50, 50);
+            }
+        }
+    }
+}
+
