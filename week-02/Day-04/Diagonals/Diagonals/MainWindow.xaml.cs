e193fae91e4c2ea9fb1d835d4c845434f5a448a1
Kurti Peter <kurtipeter90@gmail.com>
2018-05-17 12:49:03 +0200

    add(Diagonals)

---
+++ week-02/Day-04/Diagonals/Diagonals/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Diagonals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawDiagonals(foxDraw);
+
+        }
+
+        public static void DrawDiagonals(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0, 0, foxDraw.Canvas.Width, foxDraw.Canvas.Height);
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(foxDraw.Canvas.Width, 0, 0, foxDraw.Canvas.Height);
+        }
+    }
+}
