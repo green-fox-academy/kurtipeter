630b39cce51370a61ec808dc562ee653cb025a1d
Kurti Peter <kurtipeter90@gmail.com>
2018-05-23 16:41:07 +0200

    chore(Sharpie)

---
+++ week-03/Day-03/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sharpie sharpie = new Sharpie("green", 34.23f);
+            sharpie.Use();
+            Console.WriteLine(sharpie.color + " " +  sharpie.inkAmount +" "+ sharpie.width);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-03/Day-03/Sharpie/Sharpie/Sharpie..cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharpie
+{
+    class Sharpie
+    {
+        public string color;
+        public float width;
+        public float inkAmount;
+
+        public Sharpie(string color, float width )
+        {
+            this.color = color;
+            this.width = width;
+            inkAmount = 100;
+        }
+
+        public void Use()
+        {
+            inkAmount--;
+        }
+    }
+}
