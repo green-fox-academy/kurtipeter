c8fc349cb05f7c11dcc4c877452267f54d8874df
Kurti Peter <kurtipeter90@gmail.com>
2018-05-23 16:18:03 +0200

    chore(FleetOfThings)

---
+++ week-03/Day-03/FleetOfThings/FleetOfThings/Fleet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FleetOfThings
+{
+    public class Fleet
+    {
+        private List<Thing> Things;
+
+        public Fleet()
+        {
+            Things = new List<Thing>();
+        }
+
+        public void Add(Thing thing)
+        {
+            Things.Add(thing);
+        }
+
+        public string WriteFleetOut()
+        {
+            string output = "";
+
+            for (int i = 0; i < Things.Count; i++)
+            {
+                output += (i+1).ToString() + ". [";
+                if (Things[i].IsItComplete())
+                {
+                    output += "X";
+                }
+                else
+                {
+                    output += " ";
+                }
+                output += "] " + Things[i].NameOfTheThing() + "\n";
+            }
+            return output;
+        }
+    }
+}
---
+++ week-03/Day-03/FleetOfThings/FleetOfThings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FleetOfThings
+{
+    public class FleetOfThings
+    {
+        public static void Main(string[] args)
+        {
+            var fleet = new Fleet();
+            // Create a fleet of things to have this output:
+            // 1. [ ] Get milk
+            // 2. [ ] Remove the obstacles
+            // 3. [x] Stand up
+            // 4. [x] Eat lunch
+            // Hint: You have to create a Print method also
+            Thing getMilk = new Thing("Get Milk");
+            Thing remObs = new Thing("Remove the obstacles");
+            Thing standUp = new Thing("Stand up");
+            standUp.Complete();
+            Thing eatLunch = new Thing("Eat lunch");
+            eatLunch.Complete();
+
+            fleet.Add(getMilk);
+            fleet.Add(remObs);
+            fleet.Add(standUp);
+            fleet.Add(eatLunch);
+
+            Console.WriteLine(fleet.WriteFleetOut());
+            Console.ReadLine();
+
+        }
+    }
+}
