14bcbfdb91a4b817cd57b4d545eff200daede2ff
Kurti Peter <kurtipeter90@gmail.com>
2018-05-23 16:34:33 +0200

    choreAnimal

---
+++ week-03/Day-03/Animal/Animal/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    class Animal
+    {
+        public int hunger;
+        public int thirst;
+
+        public Animal()
+        {
+            hunger = 50;
+            thirst = 50;
+        }
+
+        public void Eat()
+        {
+            hunger--;
+        }
+
+        public void Drink()
+        {
+            thirst--;
+        }
+
+        public void Play()
+        {
+            hunger++;
+            thirst++;
+        }
+
+    }
+}
---
+++ week-03/Day-03/Animal/Animal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal zebraallat = new Animal();
+            Console.WriteLine(zebraallat.hunger + " " + zebraallat.thirst);
+            zebraallat.Eat();
+            zebraallat.Drink();
+            Console.WriteLine(zebraallat.hunger + " " + zebraallat.thirst);
+            zebraallat.Play();
+            Console.WriteLine(zebraallat.hunger + " " + zebraallat.thirst);
+            Console.ReadLine();
+        }
+    }
+}
