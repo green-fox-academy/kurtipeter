68a99e328a400c9d900fcad7de5fd31ed040d680
Kurti Peter <kurtipeter90@gmail.com>
2018-05-23 14:10:39 +0200

    chore(Domino)

---
+++ week-03/Day-03/Domino/Domino/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domino
+{
+    public class Dominoes
+    {
+        public static void Main(string[] args)
+        {
+            var dominoes = InitializeDominoes();
+            // You have the list of Dominoes
+            // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+            // Create a function to write the dominous to the console in the following format
+            // eg: [2, 4], [4, 3], [3, 5] ...
+            DominoSorter(dominoes);
+            Console.ReadLine();
+        }
+
+        public static List<Domino> InitializeDominoes()
+        {
+            var dominoes = new List<Domino>();
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
+            return dominoes;
+        }
+
+        public static void DominoSorter(List<Domino> dominoes)
+        {
+            List<int[]> output = new List<int[]>();
+
+            Random r = new Random();
+
+            output.Add(dominoes[r.Next(dominoes.Count)].GetValues());
+
+            for (int j = 0; j < dominoes.Count; j++)
+            {
+                for (int i = 0; i < dominoes.Count; i++)
+                {
+                    if (output[j][1] == dominoes[i].GetValues()[0])
+                    {
+                        output.Add(dominoes[i].GetValues());
+                    }
+                }
+            }
+
+            foreach (var eachDomino in output)
+            {
+                Console.Write("[");
+
+                for (int i = 0; i < eachDomino.Length; i++)
+                {
+                    Console.Write(eachDomino[i]);
+
+                    if (i != eachDomino.Length - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.Write("]");
+            }
+        }
+    }
+}
