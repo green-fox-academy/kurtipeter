cf7e806bee69d5bda255bc116a33e9ac6d19c3a0
Kurti Peter <kurtipeter90@gmail.com>
2018-05-23 12:08:50 +0200

    chore(PostIt)

---
+++ week-03/Day-03/PostIt/PostIt/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PostIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt postItOne = new PostIt("orange", "blue", "Idea 1");
+            PostIt postItTwo = new PostIt("pink", "black", "Awesome");
+            PostIt postItThree = new PostIt("yellow", "green", "Superb");
+
+            postItOne.WriteOutFields();
+            postItTwo.WriteOutFields();
+            postItThree.WriteOutFields();
+            Console.ReadLine();
+
+        }
+    }
+
+    public struct PostIt
+    {
+        string backgroundColor;
+        string textColor;
+        string text;
+
+        public PostIt(string backgroundColor, string textColor, string text)
+        {
+            this.backgroundColor = backgroundColor;
+            this.textColor = textColor;
+            this.text = text;
+        }
+
+        public void WriteOutFields()
+        {
+            string output = string.Format("Background color: {0}, Textcolor: {1}, Text: {2}", backgroundColor, textColor, text);
+            Console.WriteLine(output);
+        }
+    }
+}
