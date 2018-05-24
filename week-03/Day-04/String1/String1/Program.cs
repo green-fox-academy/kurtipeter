976527f578e165e35955e83bbc5c3d44c6af4d65
Kurti Peter <kurtipeter90@gmail.com>
2018-05-24 11:41:02 +0200

    chore(String1)

---
+++ week-03/Day-04/String1/String1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace String1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+            Console.WriteLine(TheXChanger("pussx"));
+            Console.ReadLine();
+        }
+
+        public static string TheXChanger(string input)
+        {
+            if (input == "")
+            {
+                return null;
+            }
+
+            else
+            {
+                char firstLetter;
+
+                if (input[0] == 'x')
+                {
+                    firstLetter = 'y';
+                }
+
+                else
+                {
+                    firstLetter = input[0];
+                }
+
+                return firstLetter + TheXChanger(input.Remove(0, 1));
+            }
+        }
+    }
+}
