cdbb02212585101cd8e483e37215d2e9f06f87a6
Kurti Peter <kurtipeter90@gmail.com>
2018-05-24 12:37:35 +0200

    chore(Refactorio)

---
+++ week-03/Day-04/Refactorio/Refactorio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Refactorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a recursive function called `refactorio`
+            // that returns it's input's factorial
+            Console.WriteLine(Factorer(4));
+            Console.ReadLine();
+        }
+
+        public static int Factorer(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return Factorer(n - 1) * n;
+        }
+    }
+}
