9b5463c5930521c788afc1ab61b7d64464f00adb
Kurti Peter <kurtipeter90@gmail.com>
2018-05-24 11:16:14 +0200

    chore(Bunny2)

---
+++ week-03/Day-04/Bunny2/Bunny2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Bunny2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+            // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+            // have 3 ears, because they each have a raised foot. Recursively return the
+            // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+            Console.WriteLine(WeirdBunnyEarCounter(1));
+            Console.WriteLine(WeirdBunnyEarCounter(2));
+            Console.WriteLine(WeirdBunnyEarCounter(3));
+            Console.WriteLine(WeirdBunnyEarCounter(4));
+            Console.ReadLine();
+        }
+
+        public static int WeirdBunnyEarCounter(int numberOfBunnies)
+        {
+
+            if (numberOfBunnies == 0)
+            {
+                return 0;
+            }
+
+            else
+            {
+                int bunnyEarNumber = 0;
+
+                if (numberOfBunnies % 2 == 1)
+                {
+                    bunnyEarNumber = 2;
+                }
+                else if (numberOfBunnies % 2 == 0)
+                {
+                    bunnyEarNumber = 3;
+                }
+
+                return WeirdBunnyEarCounter(numberOfBunnies - 1) + bunnyEarNumber;
+            }
+        }
+    }
+}
