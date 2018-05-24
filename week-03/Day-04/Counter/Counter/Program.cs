1d4887a0388314d287bbecdbe4d33102d3b433e2
Kurti Peter <kurtipeter90@gmail.com>
2018-05-24 10:31:31 +0200

    chore(Counter)

---
+++ week-03/Day-04/Counter/Counter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Counter(12));
+            Console.ReadLine();
+        }
+
+        public static int Counter(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Counter(n - 1);
+            }
+        }
+    }
+}
