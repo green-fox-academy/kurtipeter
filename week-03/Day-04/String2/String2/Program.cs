913313c120e6668dc5691d0835c4fac50189dbde
Kurti Peter <kurtipeter90@gmail.com>
2018-05-24 12:13:57 +0200

    chore(String2)

---
+++ week-03/Day-04/String2/String2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace String2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+            Console.WriteLine(TheXRemover2("xpxexnxixsx"));
+            Console.ReadLine();
+        }
+
+        public static string TheXRemover2(string input)
+        {
+            if (input == "")
+            {
+                return null;
+            }
+
+            else
+            {
+                if (input[0] == 'x')
+                {
+                    return TheXRemover2(input.Remove(0, 1));
+                }
+
+                return input[0] + TheXRemover2(input.Remove(0, 1));
+            }
+        }
+    }
+}
