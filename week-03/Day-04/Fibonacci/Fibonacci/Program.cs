291be782afaf99f3b7171378fe78b084f63fdaf0
Kurti Peter <kurtipeter90@gmail.com>
2018-05-24 12:45:14 +0200

    chore(Fibonacci)

---
+++ week-03/Day-04/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibo(6));
+            Console.ReadLine();
+        }
+
+        public static int Fibo(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+
+            return Fibo(n - 1) + Fibo(n - 2);
+        }
+    }
+}
