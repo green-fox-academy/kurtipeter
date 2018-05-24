c17f8a727ec42d59b3a4f1bdf4258e6c8ab4a59b
Kurti Peter <kurtipeter90@gmail.com>
2018-05-24 11:03:41 +0200

    chore(Bunny1)

---
+++ week-03/Day-04/Bunny1/Bunny1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bunny1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+            Console.WriteLine(BunnyEarCounter(12));
+            Console.ReadLine();
+        }
+
+        public static int BunnyEarCounter(int numberOfBunnies)
+        {
+            if (numberOfBunnies == 1)
+            {
+                return 2;
+            }
+            else
+            {
+                return BunnyEarCounter(numberOfBunnies - 1) + 2;
+            }
+        }
+
+    }
+}
