a53fab6d1a6d612b58625a5d1d068f393a27f421
Kurti Peter <kurtipeter90@gmail.com>
2018-06-18 14:55:41 +0200

    chore(Exercise10)

---
+++ week-06/Day-01/Exercise10/Exercise10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fox> foxes = new List<Fox>();
+            foxes.Add(new Fox { Name = "Joci", Color = "green", Type = "palsdfdsf" });
+            foxes.Add(new Fox { Name = "Miyu", Color = "yellow", Type = "pdfdf" });
+            foxes.Add(new Fox { Name = "Lulu", Color = "blue", Type = "vulpies" });
+            foxes.Add(new Fox { Name = "Popo", Color = "green", Type = "pallida" });
+            foxes.Add(new Fox { Name = "Kiki", Color = "green", Type = "pallida" });
+
+            var greenFox = from fox in foxes
+                           where fox.Color == "green"
+                           select fox;
+
+            var greenPallidaFox = from fox in foxes
+                           where fox.Color == "green" && fox.Type == "pallida"
+                           select fox;
+
+            foreach (var item in greenFox)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in greenPallidaFox)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Fox
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+    }
+}
