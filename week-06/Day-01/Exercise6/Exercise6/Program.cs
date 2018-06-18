13370956a2e61f995b997a41f2bf1a76c9405fbe
Kurti Peter <kurtipeter90@gmail.com>
2018-06-18 13:01:38 +0200

    chore(Exercise6)

---
+++ week-06/Day-01/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "Peetiiii";
+            Console.WriteLine(testString.FindFreqOfChar('e'));
+            Console.WriteLine(testString.FindFreqOfChar2('i'));
+            Console.ReadLine();
+        }
+
+        public static int FindFreqOfChar(this String inputString, char character)
+        {
+            var queryTest = inputString.ToLower().Where(k => k == character).Count();
+            return queryTest;
+        }
+
+        public static int FindFreqOfChar2(this String inputString, char character)
+        {
+            var methodTest = (from c in inputString.ToLower()
+                             where c == character
+                             select c).Count();
+            return methodTest;
+        }
+    }
+}
