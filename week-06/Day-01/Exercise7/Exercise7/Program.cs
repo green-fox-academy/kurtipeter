473218d3b96b736c1cd823c9dd46a7c2d15b0fd7
Kurti Peter <kurtipeter90@gmail.com>
2018-06-18 13:11:20 +0200

    chore(Exercise7)

---
+++ week-06/Day-01/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS", "AskjdfnsdnfI" };
+            IEnumerable<string> solution = FindCity(cities);
+            foreach (var sol in solution)
+            {
+                Console.WriteLine(sol);
+            }
+            Console.ReadLine();
+        }
+
+        static public IEnumerable<string> FindCity(string[] cities)
+        {
+            var output = from city in cities
+                         where city[0] == 'A' && city[city.Length - 1] == 'I'
+                         select city;
+            return output;
+        }
+    }
+}
