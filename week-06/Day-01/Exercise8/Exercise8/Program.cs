70337e8e19c013712b7332c7b1a72f03e203193c
Kurti Peter <kurtipeter90@gmail.com>
2018-06-18 13:38:26 +0200

    chore(Exercise8)

---
+++ week-06/Day-01/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the uppercase characters in a string!
+            string test = "kPutyaE3Ncsksdf1IaS";
+            foreach (var item in test.FindUpperCase())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<char> FindUpperCase(this String inputS)
+        {
+            var output = from c in inputS
+                         where char.IsUpper(c)
+                         select c;
+            return output;
+        }
+    }
+}
