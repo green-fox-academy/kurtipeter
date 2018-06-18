845d38a7c88ce98a64637f083c68e742e6739a7f
Kurti Peter <kurtipeter90@gmail.com>
2018-06-18 15:42:21 +0200

    chore(Exercise9)

---
+++ week-06/Day-01/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] test = new char[] { 'k', 'u', 't', 'y', 'a' };
+
+            string convertToString = new string(test);
+
+            string converter = string.Join("", test.Select(s => s));
+
+            Console.WriteLine(converter);
+            Console.WriteLine(convertToString);
+            Console.ReadLine();
+
+        }
+    }
+}
