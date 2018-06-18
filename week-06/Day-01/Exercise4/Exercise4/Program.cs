f036be1d4806aeff72d29ef72a3a92345e644644
Kurti Peter <kurtipeter90@gmail.com>
2018-06-18 11:17:54 +0200

    chore(Exercise4)

---
+++ week-06/Day-01/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var queryTest = from num2 in (from num in n select num * num)
+                            where num2 > 20
+                            select num2;
+
+            foreach (var item in queryTest)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            var methodTest = n.Select(num => num * num).Where(num2 => num2 > 20);
+
+            foreach (var item in queryTest)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
