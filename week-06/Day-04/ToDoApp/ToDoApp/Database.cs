148a68d8b8acb0d145481c8c5fef66a4ad67479a
Kurti Peter <kurtipeter90@gmail.com>
2018-06-21 13:37:03 +0200

    chore(ToDoApp)

---
+++ week-06/Day-04/ToDoApp/ToDoApp/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace ToDoApp
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source=database.sqlite3");
+
+            if (!File.Exists("./database.sqlite3"))
+            {
+               SQLiteConnection.CreateFile("database.sqlite3");
+                Console.WriteLine("Database added.");
+            }
+        }
+
+        public void WriteInDatabase()
+        {
+            string query = "INSERT INTO user1 ('Description','Done') VALUES(@Description, @Done)";
+        }
+    }
+}
---
+++ week-06/Day-04/ToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database databaseObj = new Database();
+            Console.ReadLine();
+        }
+    }
+}
