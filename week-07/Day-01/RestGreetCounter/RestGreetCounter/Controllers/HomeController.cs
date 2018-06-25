856925e845dca831ee5a43259485595ce5106701
Kurti Peter <kurtipeter90@gmail.com>
2018-06-25 14:14:15 +0200

    chore(RestGreetCounter)

---
+++ week-07/Day-01/RestGreetCounter/RestGreetCounter/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestGreetCounter.Modules;
+
+namespace RestGreetCounter.Controllers
+{
+    [Route("api")]
+    public class HomeController : Controller
+    {
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            Greeting greet = new Greeting() { Name = "Hello Wolrd " + name };
+            return new JsonResult(greet);
+        }
+    }
+}
---
+++ week-07/Day-01/RestGreetCounter/RestGreetCounter/Modules/Greeting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestGreetCounter.Modules
+{
+    public class Greeting
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private static int autoID = 1;
+
+        public Greeting()
+        {
+            Id = autoID;
+            autoID++;
+        }
+    }
+}
