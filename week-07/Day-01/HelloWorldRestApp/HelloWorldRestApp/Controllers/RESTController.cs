83b47424b11e6da0d882c82c4bf88e40629e0e24
Kurti Peter <kurtipeter90@gmail.com>
2018-06-25 13:56:10 +0200

    chore(HelloWorldWeb)

---
+++ week-07/Day-01/HelloWorldRestApp/HelloWorldRestApp/Controllers/RESTController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorldRestApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldRestApp.Controllers
+{
+    [Route("api")]
+
+    public class RESTController : Controller
+    {
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            Greeting greet = new Greeting() { Id = 1, Content = "Hello World " + name };
+            return new JsonResult(greet);
+        }
+
+        [Route("greeting/{name}")]
+        public IActionResult Greeting1([FromRoute]string name)
+        {
+            Greeting greet = new Greeting() { Id = 1, Content = "Hello World " + name };
+            return new JsonResult(greet);
+        }
+    }
+}
